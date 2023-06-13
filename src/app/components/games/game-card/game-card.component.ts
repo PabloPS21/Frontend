625c447a212fb58682fa4b415f86af8fc3e498b0
PabloPS21 <pabloserrano.est21@gmail.com>
2023-06-13 18:45:52 +0200

    Pantalla de detalles de videojuego

--- src/app/components/games/game-card/game-card.component.ts
+++ src/app/components/games/game-card/game-card.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input } from '@angular/core';
-import { CardGame } from 'src/app/models/cardGame';
+import { Component, Input, OnInit} from '@angular/core';
+import { Result } from 'src/app/models/searchGame';
 
 @Component({
   selector: 'app-game-card',
   templateUrl: './game-card.component.html',
   styleUrls: ['./game-card.component.css']
 })
-export class GameCardComponent {
+export class GameCardComponent{
   
-  @Input() juego!: CardGame;
+  @Input() juego!: Result;
+
 }
