336952734e8e8be91bc0e8b3e0db96d5d9bb8126
PabloPS21 <pabloserrano.est21@gmail.com>
2023-06-17 17:20:40 +0200

    api key

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     gameApi_key2: '21b066e20f8d4732972bff635a46e79e',
-    gameApi_key: '6a8fdf0573244ffa8c9b14aa61f50592'
+    gameApi_key: 'f8af7fcf2e394326b9989a55d9066160'
 }
