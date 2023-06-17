42f999cfa556db03da28b1844de60649afce8b52
PabloPS21 <pabloserrano.est21@gmail.com>
2023-06-17 12:19:41 +0200

    nueva api key

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    gameApi_key: '21b066e20f8d4732972bff635a46e79e'
+    gameApi_key2: '21b066e20f8d4732972bff635a46e79e',
+    gameApi_key: '6a8fdf0573244ffa8c9b14aa61f50592'
 }
